60e07bc63950c27e7cd4a8aba98cff21e4ed3b79
Shondanishea Edwards <shondaxavier2@gmail.com>
2021-02-13 10:18:31 -0600

    week2.js

--- week2.js
+++ week2.js
@@ -1,9 +1,20 @@
-const pizzaPlace;
-let numberOfToppings;
+const pizzaPlace ="Anthony's Pizza";
+let numberOfToppings = 3;
 
-pizzaPlace = "Anthony's Pizza";
-numberOfToppings = 3;
+numberOfToppings = 6;
 
-console.log(typeOf pizzaPlace);
-console.log(typeOf numberOfToppings);
+console.log(typeof pizzaPlace);
+console.log(typeof numberOfToppings);
 
+console.log(`"I love ${pizzaPlace}!," They have "${numberOfToppings} toppings."`);
+
+function pizza(numberOfToppings){
+
+if  (numberOfToppings <= 5){
+  console.log("Quality, not quantity.");
+}else{
+  console.log("A whole lot of pizza.");
+}
+}
+pizza(3);
+pizza(6);
